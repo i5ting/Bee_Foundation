85cb667bc9dc5ed78f006396959a933d501b11c1
i5ting <i5ting@126.com>
2013-04-16 11:02:32 +0800

    Initial Commit

---
+++ Bee_FoundationTests/Bee_FoundationTests.h
@@ -0,0 +1,13 @@
+//
+//  Bee_FoundationTests.h
+//  Bee_FoundationTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Bee_FoundationTests : SenTestCase
+
+@end
---
+++ Bee_FoundationTests/Bee_FoundationTests.m
@@ -0,0 +1,32 @@
+//
+//  Bee_FoundationTests.m
+//  Bee_FoundationTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import "Bee_FoundationTests.h"
+
+@implementation Bee_FoundationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Bee_FoundationTests");
+}
+
+@end
